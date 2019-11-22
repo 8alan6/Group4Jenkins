aece5e923e7be51141aacdf5f4fb3d9c8305fb41
Alan Walsh <a.walsh@mycit.ie>
2019-11-22 00:01:43 +0000

    Changed Greeting

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python -u
 import datetime
-print("Hello world!")
+print("It is working :) ")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
