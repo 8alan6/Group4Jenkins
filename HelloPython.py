588abd7a7973d4773e36cd8900ba39c6625d9fc3
Alan Walsh <a.walsh@mycit.ie>
2019-12-11 18:20:11 +0000

    Update: 2

--- HelloPython.py
+++ HelloPython.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python -u
 import datetime
-print("Group4Jenkins")
+print("Group4Jenkins Updated:2")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
